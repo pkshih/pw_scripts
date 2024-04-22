c33e0482cdb919e65ff8bbdbfadc519df4fea6e8
Ping-Ke Shih <pkshih@realtek.com>
2024-04-22 09:57:35 +0800

    pw_check_top: add prompt for revert comit
    Signed-off-by: Ping-Ke Shih <pkshih@realtek.com>

--- pw_check_top.sh
+++ pw_check_top.sh
@@ -13,6 +13,7 @@
 subject_sha1=`git log --oneline -1 --decorate=no`
 subject=`echo $subject_sha1 | cut -f 2- -d ' '`
 
+revert=`echo $subject | grep "^Revert \""`
 match=`echo $subject | sed -n "s/\(wifi: \(rtw89\|rtw88\|rtlwifi\|rtl8xxxu\)\): .*/\2/p"`
 dirmatch=`git diff HEAD^ --name-only | grep drivers/net/wireless/realtek/$match`
 extra_msg=""
@@ -21,6 +22,7 @@ extra_msg=""
 if [ "$match" == "" ] || [ "$dirmatch" == "" ]; then
 	echo -e "\e[0;33minvalid subject: $subject $extra_msg\e[0m"
 	if [ "$PWDRY" == "" ]; then
+		[ "$revert" != "" ] && echo -e "\e[0;34msuggest to continue because of revert commit.\e[0m"
 		echo -n "Continue? (y) "
 		read y && [ "$y" != "y" ] && exit 1
 	fi
